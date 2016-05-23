b4f2d058223fdb1011400abf83b7d8534632da01
Vasilis Spilka <vasspilka@gmail.com>
2016-05-23 21:56:44 +0300

    we love the space

--- lib/hanami/fumikiri.rb
+++ lib/hanami/fumikiri.rb
@@ -1,11 +1,11 @@
 module Hanami
   module Fumikiri
-		module Skip
-	    private
-	    def authenticate!
+    module Skip
+      private
+      def authenticate!
         # no-op implementation
-	    end
-	  end
+      end
+    end
 
     class InvalidTokenError < StandardError
       def message
@@ -13,42 +13,42 @@ module Hanami
       end
     end
 
-		def self.included(base)
-			base.class_eval do
-				before :authenticate!
-				expose :current_user
-			end
-		end
+    def self.included(base)
+      base.class_eval do
+        before :authenticate!
+        expose :current_user
+      end
+    end
 
-		def current_user
-			validate_jwt
-			@current_user = UserRepository.find(@decoded_token['user_id'])
-		end
+    def current_user
+      validate_jwt
+      @current_user = UserRepository.find(@decoded_token['user_id'])
+    end
 
-		private
-		def authenticate!
-			redirect_to '/login' unless authenticated?
-		end
+    private
+    def authenticate!
+      redirect_to '/login' unless authenticated?
+    end
 
-		def authenticated?
-			! current_user.nil?
-		end
+    def authenticated?
+      ! current_user.nil?
+    end
 
-		def validate_jwt
-			begin
-				auth = request.headers['Authorisation']
+    def validate_jwt
+      begin
+        auth = request.headers['Authorisation']
         # make better errors
-				raise InvalidTokenError if auth.nil?
+        raise InvalidTokenError if auth.nil?
 
-				token = auth.split(' ').last
-				@decoded_token = JWT.decode(token, ENV['JWT_SECRET'])
+        token = auth.split(' ').last
+        @decoded_token = JWT.decode(token, ENV['JWT_SECRET'])
         # make better errors
-				raise InvalidTokenError if @decoded_token['user_id'].empty?
+        raise InvalidTokenError if @decoded_token['user_id'].empty?
 
-			rescue	JWT::DecodeError
+      rescue JWT::DecodeError
         # make better errors
-				raise InvalidTokenError
-			end
-		end
+        raise InvalidTokenError
+      end
+    end
   end
 end
