789d75fd5a80b1fd8192aecf93d800faa04ef856
Vasilis Spilka <vasspilka@gmail.com>
2016-05-24 12:22:12 +0300

    better descriptions

--- spec/hanami-fumikiri/authorisation_header_spec.rb
+++ spec/hanami-fumikiri/authorisation_header_spec.rb
@@ -10,12 +10,12 @@ describe Hanami::Fumikiri do
     end
   end
 
-  it 'raises MissingTokenError when Authorisation is nil' do
+  it 'raises MissingTokenError when Authorisation is missing' do
     expect{ action.new.call({}) }.to raise_error \
       Hanami::Fumikiri::MissingTokenError
   end
 
-  it 'should rails invalid token error' do
+  it 'raises error when invalid token' do
     expect{ action.new.call('Authorisation' => 'not_so_valid_token') }.to raise_error \
       Hanami::Fumikiri::InvalidTokenError
   end
