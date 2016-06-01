bea8e4e36ba1d1fada08edbacd878d53f1722409
Vasilis Spilka <vasspilka@gmail.com>
2016-06-01 20:11:17 +0300

    remove custom errors

--- lib/hanami/fumikiri/errors.rb
+++ lib/hanami/fumikiri/errors.rb
@@ -1,30 +1,11 @@
 module Hanami
   module Fumikiri
 
-    class InvalidTokenError < StandardError
-      def message
-        "Your token has problems... "
-      end
-    end
-
     class MissingTokenError < StandardError
       def message
         "No 'Authentication' header provided."
       end
     end
-
-    class MissingSubError < StandardError
-      def message
-        "Token has no 'sub' attribute."
-      end
-    end
-
-    class MissingUserError < StandardError
-      def message
-        "Could not find user."
-      end
-    end
-
 
   end
 end
