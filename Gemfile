9a5791c6c14f1371eb74e5f1deb690eb6039e746
The Crab <nelson@safarista.com>
2016-08-21 00:58:33 +0100

    Fix travis requires

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 gemspec
 gem 'hanami-utils', github: 'hanami/utils', branch: 'master'
 gem 'hanami-model', github: 'hanami/model', branch: 'master'
+gem 'coveralls', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@ require 'hanami-fumikiri'
 require 'hanami/controller'
 require 'hanami/model'
 require_relative 'hanami-fumikiri/fixtures'
+require 'coveralls'
+
+Coveralls.wear!
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
@@ -12,21 +15,19 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-=begin
-  config.filter_run :focus
-  config.run_all_when_everything_filtered = true
-
-  config.example_status_persistence_file_path = "spec/examples.txt"
-  config.disable_monkey_patching!
-  config.warnings = true
-
-  if config.files_to_run.one?
-    config.default_formatter = 'doc'
-  end
-
-  config.profile_examples = 10
-  config.order = :random
-
-  Kernel.srand config.seed
-=end
+  # begin
+  #   config.filter_run :focus
+  #   config.run_all_when_everything_filtered = true
+  #
+  #   config.example_status_persistence_file_path = 'spec/examples.txt'
+  #   config.disable_monkey_patching!
+  #   config.warnings = true
+  #
+  #   config.default_formatter = 'doc' if config.files_to_run.one?
+  #
+  #   config.profile_examples = 10
+  #   config.order = :random
+  #
+  #   Kernel.srand config.seed
+  # end
 end
